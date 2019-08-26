079749aaeede20bc149acb1e93c4e8a924b5355e
Jens Peters <jp7677@gmail.com>
2019-08-26 22:02:35 +0200

    Rename variables and remove not needed statements.

--- package-release.sh
+++ package-release.sh
@@ -9,13 +9,12 @@ if [ -z "$1" ] || [ -z "$2" ]; then
   exit 1
 fi
 
-DXVK_VERSION="$1"
-DXVK_SRC_DIR=`dirname $(readlink -f $0)`
-DXVK_BUILD_DIR=$(realpath "$2")"/dxvk-$DXVK_VERSION"
-DXVK_ARCHIVE_PATH=$(realpath "$2")"/dxvk-$DXVK_VERSION.tar.gz"
+VERSION="$1"
+SRC_DIR=`dirname $(readlink -f $0)`
+BUILD_DIR=$(realpath "$2")"/dxvk-nvapi-$VERSION"
 
-if [ -e "$DXVK_BUILD_DIR" ]; then
-  echo "Build directory $DXVK_BUILD_DIR already exists"
+if [ -e "$BUILD_DIR" ]; then
+  echo "Build directory $BUILD_DIR already exists"
   exit 1
 fi
 
@@ -43,26 +42,23 @@ done
 
 
 function build_arch {
-  export WINEARCH="win$1"
-  export WINEPREFIX="$DXVK_BUILD_DIR/wine.$1"
-  
-  cd "$DXVK_SRC_DIR"
+  cd "$SRC_DIR"
 
-  meson --cross-file "$DXVK_SRC_DIR/$crossfile$1.txt" \
+  meson --cross-file "$SRC_DIR/$crossfile$1.txt" \
         --buildtype "release"                         \
-        --prefix "$DXVK_BUILD_DIR"                    \
+        --prefix "$BUILD_DIR"                    \
         --strip                                       \
         --bindir "x$1"                                \
         --libdir "x$1"                                \
-        "$DXVK_BUILD_DIR/build.$1"
+        "$BUILD_DIR/build.$1"
 
-  cd "$DXVK_BUILD_DIR/build.$1"
+  cd "$BUILD_DIR/build.$1"
   ninja install
 
   if [ $opt_devbuild -eq 0 ]; then
     # get rid of some useless .a files
-    rm "$DXVK_BUILD_DIR/x$1/"*.!(dll)
-    rm -R "$DXVK_BUILD_DIR/build.$1"
+    rm "$BUILD_DIR/x$1/"*.!(dll)
+    rm -R "$BUILD_DIR/build.$1"
   fi
 }
 
