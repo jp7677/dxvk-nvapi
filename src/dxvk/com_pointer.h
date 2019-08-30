af70ca2dd02f7166afd0cf797545a278f66c5721
Jens Peters <jp7677@gmail.com>
2019-08-30 08:53:39 +0200

    Add Com<T> from DXVK. Taken from https://github.com/doitsujin/dxvk/blob/master/src/util/com/com_pointer.h

---
+++ src/dxvk/com_pointer.h
@@ -0,0 +1,118 @@
+#pragma once
+
+// GCC complains about the COM interfaces
+// not having virtual destructors
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#endif // __GNUC__
+
+namespace dxvk {
+  
+  /**
+   * \brief COM pointer
+   * 
+   * Implements automatic reference
+   * counting for COM objects.
+   */
+  template<typename T>
+  class Com {
+    
+  public:
+    
+    Com() { }
+    Com(std::nullptr_t) { }
+    Com(T* object)
+    : m_ptr(object) {
+      this->incRef();
+    }
+    
+    Com(const Com& other)
+    : m_ptr(other.m_ptr) {
+      this->incRef();
+    }
+    
+    Com(Com&& other)
+    : m_ptr(other.m_ptr) {
+      other.m_ptr = nullptr;
+    }
+    
+    Com& operator = (T* object) {
+      this->decRef();
+      m_ptr = object;
+      this->incRef();
+      return *this;
+    }
+    
+    Com& operator = (const Com& other) {
+      other.incRef();
+      this->decRef();
+      m_ptr = other.m_ptr;
+      return *this;
+    }
+    
+    Com& operator = (Com&& other) {
+      this->decRef();
+      this->m_ptr = other.m_ptr;
+      other.m_ptr = nullptr;
+      return *this;
+    }
+    
+    Com& operator = (std::nullptr_t) {
+      this->decRef();
+      m_ptr = nullptr;
+      return *this;
+    }
+    
+    ~Com() {
+      this->decRef();
+    }
+    
+    T* operator -> () const {
+      return m_ptr;
+    }
+    
+    T**       operator & ()       { return &m_ptr; }
+    T* const* operator & () const { return &m_ptr; }
+    
+    bool operator == (const Com<T>& other) const { return m_ptr == other.m_ptr; }
+    bool operator != (const Com<T>& other) const { return m_ptr != other.m_ptr; }
+    
+    bool operator == (const T* other) const { return m_ptr == other; }
+    bool operator != (const T* other) const { return m_ptr != other; }
+    
+    bool operator == (std::nullptr_t) const { return m_ptr == nullptr; }
+    bool operator != (std::nullptr_t) const { return m_ptr != nullptr; }
+    
+    T* ref() const {
+      this->incRef();
+      return m_ptr;
+    }
+    
+    T* ptr() const {
+      return m_ptr;
+    }
+    
+  private:
+    
+    T* m_ptr = nullptr;
+    
+    void incRef() const {
+      if (m_ptr != nullptr)
+        m_ptr->AddRef();
+    }
+    
+    void decRef() const {
+      if (m_ptr != nullptr)
+        m_ptr->Release();
+    }
+    
+  };
+  
+  template<typename T>
+  T* ref(T* object) {
+    if (object != nullptr)
+      object->AddRef();
+    return object;
+  }
+  
+}
