9e6acdc6337d7dbaf4404af622b63fcacac4e37e
Jens Peters <jp7677@gmail.com>
2020-07-11 09:40:28 +0200

    Improve query interface implemenation. Building a map with function pointers (hopefully) improves query times when the same method is queried multiple times. May be this isn't needed at all due to consumers (like UE4) just keeping the function pointers and not querying over and over again.

--- src/nvapi.cpp
+++ src/nvapi.cpp
@@ -90,7 +90,15 @@ extern "C" {
         return NVAPI_OK;
     }
 
+    typedef NvAPI_Status __cdecl (*NvapiMethod) (void); 
+    static std::unordered_map<NvU32, NvapiMethod> registry;
+
     NVAPI_QUERYINTERFACE nvapi_QueryInterface(NvU32 id) {
+        auto entry = registry.find(id);
+        if (entry != registry.end()) {
+            return NVAPI_CAST(entry->second);
+        }
+
         auto it = std::find_if(
             std::begin(nvapi_interface_table),
             std::end(nvapi_interface_table),
@@ -98,29 +106,30 @@ extern "C" {
 
         if (it == std::end(nvapi_interface_table)) {
             std::cerr << "NvAPI_QueryInterface 0x" << std::hex << id << ": Called with unknown id" << std::endl;
-            return nullptr;
+            registry.insert(std::make_pair(id, (NvapiMethod) nullptr));
+        } else if (std::string(it->func) == "NvAPI_D3D11_SetDepthBoundsTest")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D11_SetDepthBoundsTest));
+        else if (std::string(it->func) == "NvAPI_D3D11_IsNvShaderExtnOpCodeSupported")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D11_IsNvShaderExtnOpCodeSupported));
+        else if (std::string(it->func) == "NvAPI_D3D_GetObjectHandleForResource")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_GetObjectHandleForResource));
+        else if (std::string(it->func) == "NvAPI_D3D_SetResourceHint")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_SetResourceHint));
+        else if (std::string(it->func) == "NvAPI_D3D_GetCurrentSLIState")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_GetCurrentSLIState));
+        else if (std::string(it->func) == "NvAPI_DISP_GetDisplayIdByDisplayName")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_DISP_GetDisplayIdByDisplayName));
+        else if (std::string(it->func) == "NvAPI_GetErrorMessage")
+            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_GetErrorMessage));
+        else if (std::string(it->func) == "NvAPI_Unload")
+            registry.insert(std::make_pair(id, NvAPI_Unload));
+        else if (std::string(it->func) == "NvAPI_Initialize")
+            registry.insert(std::make_pair(id, NvAPI_Initialize));
+        else {
+            std::cerr << "NvAPI_QueryInterface " << it->func << ": Called with not implemented method" << std::endl;
+            registry.insert(std::make_pair(id, (NvapiMethod) nullptr));
         }
 
-        if (std::string(it->func) == "NvAPI_Unload")
-            return NVAPI_CAST(NvAPI_Unload);
-        if (std::string(it->func) == "NvAPI_Initialize")
-            return NVAPI_CAST(NvAPI_Initialize);
-        if (std::string(it->func) == "NvAPI_D3D11_SetDepthBoundsTest")
-            return NVAPI_CAST(NvAPI_D3D11_SetDepthBoundsTest);
-        if (std::string(it->func) == "NvAPI_D3D11_IsNvShaderExtnOpCodeSupported")
-            return NVAPI_CAST(NvAPI_D3D11_IsNvShaderExtnOpCodeSupported);
-        if (std::string(it->func) == "NvAPI_D3D_GetObjectHandleForResource")
-            return NVAPI_CAST(NvAPI_D3D_GetObjectHandleForResource);
-        if (std::string(it->func) == "NvAPI_D3D_SetResourceHint")
-            return NVAPI_CAST(NvAPI_D3D_SetResourceHint);
-        if (std::string(it->func) == "NvAPI_D3D_GetCurrentSLIState")
-            return NVAPI_CAST(NvAPI_D3D_GetCurrentSLIState);
-        if (std::string(it->func) == "NvAPI_DISP_GetDisplayIdByDisplayName")
-            return NVAPI_CAST(NvAPI_DISP_GetDisplayIdByDisplayName);
-        if (std::string(it->func) == "NvAPI_GetErrorMessage")
-            return NVAPI_CAST(NvAPI_GetErrorMessage);
-
-        std::cerr << "NvAPI_QueryInterface " << it->func << ": Called with not implemented method" << std::endl;
-        return nullptr;
+        return NVAPI_CAST(registry.find(id)->second);
     }
 }
