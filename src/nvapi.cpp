2c4455fe7ed4bb79a27e690e712cae424c0cb86a
Jens Peters <jp7677@gmail.com>
2020-07-11 10:39:03 +0200

    Split implementation.

--- src/nvapi.cpp
+++ src/nvapi.cpp
@@ -89,47 +89,4 @@ extern "C" {
         std::cerr << "NvAPI_Initialize DXVK-NVAPI-" << DXVK_NVAPI_VERSION << ": OK" << std::endl;
         return NVAPI_OK;
     }
-
-    typedef NvAPI_Status __cdecl (*NvapiMethod) (void); 
-    static std::unordered_map<NvU32, NvapiMethod> registry;
-
-    NVAPI_QUERYINTERFACE nvapi_QueryInterface(NvU32 id) {
-        auto entry = registry.find(id);
-        if (entry != registry.end()) {
-            return NVAPI_CAST(entry->second);
-        }
-
-        auto it = std::find_if(
-            std::begin(nvapi_interface_table),
-            std::end(nvapi_interface_table),
-            [id](const auto& item) { return item.id == id; });
-
-        if (it == std::end(nvapi_interface_table)) {
-            std::cerr << "NvAPI_QueryInterface 0x" << std::hex << id << ": Called with unknown id" << std::endl;
-            registry.insert(std::make_pair(id, (NvapiMethod) nullptr));
-        } else if (std::string(it->func) == "NvAPI_D3D11_SetDepthBoundsTest")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D11_SetDepthBoundsTest));
-        else if (std::string(it->func) == "NvAPI_D3D11_IsNvShaderExtnOpCodeSupported")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D11_IsNvShaderExtnOpCodeSupported));
-        else if (std::string(it->func) == "NvAPI_D3D_GetObjectHandleForResource")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_GetObjectHandleForResource));
-        else if (std::string(it->func) == "NvAPI_D3D_SetResourceHint")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_SetResourceHint));
-        else if (std::string(it->func) == "NvAPI_D3D_GetCurrentSLIState")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_D3D_GetCurrentSLIState));
-        else if (std::string(it->func) == "NvAPI_DISP_GetDisplayIdByDisplayName")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_DISP_GetDisplayIdByDisplayName));
-        else if (std::string(it->func) == "NvAPI_GetErrorMessage")
-            registry.insert(std::make_pair(id, (NvapiMethod) NvAPI_GetErrorMessage));
-        else if (std::string(it->func) == "NvAPI_Unload")
-            registry.insert(std::make_pair(id, NvAPI_Unload));
-        else if (std::string(it->func) == "NvAPI_Initialize")
-            registry.insert(std::make_pair(id, NvAPI_Initialize));
-        else {
-            std::cerr << "NvAPI_QueryInterface " << it->func << ": Called with not implemented method" << std::endl;
-            registry.insert(std::make_pair(id, (NvapiMethod) nullptr));
-        }
-
-        return NVAPI_CAST(registry.find(id)->second);
-    }
 }
