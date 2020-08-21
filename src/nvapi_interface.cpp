729b00dbbd84475bd015517b2df985b32d580d71
Jens Peters <jp7677@gmail.com>
2020-08-21 10:32:16 +0200

    Use macro to help with stringification.

--- src/nvapi_interface.cpp
+++ src/nvapi_interface.cpp
@@ -5,6 +5,11 @@
 #pragma GCC diagnostic ignored "-Wcast-function-type"
 #endif // __GNUC__
 
+#define INSERT_WHEN_EQUALS(method) \
+    if (std::string(it->func) == #method) { \
+        registry.insert(std::make_pair(id, (NvAPI_Method) method)); \
+    }
+
 extern "C" {
     using namespace dxvk;
     using NvAPI_Method = NvAPI_Status (*) ();
@@ -25,24 +30,16 @@ extern "C" {
         if (it == std::end(nvapi_interface_table)) {
             std::cerr << "NvAPI_QueryInterface 0x" << std::hex << id << ": Called with unknown id" << std::endl;
             registry.insert(std::make_pair(id, (NvAPI_Method) nullptr));
-        } else if (std::string(it->func) == "NvAPI_D3D11_SetDepthBoundsTest")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_D3D11_SetDepthBoundsTest));
-        else if (std::string(it->func) == "NvAPI_D3D11_IsNvShaderExtnOpCodeSupported")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_D3D11_IsNvShaderExtnOpCodeSupported));
-        else if (std::string(it->func) == "NvAPI_D3D_GetObjectHandleForResource")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_D3D_GetObjectHandleForResource));
-        else if (std::string(it->func) == "NvAPI_D3D_SetResourceHint")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_D3D_SetResourceHint));
-        else if (std::string(it->func) == "NvAPI_D3D_GetCurrentSLIState")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_D3D_GetCurrentSLIState));
-        else if (std::string(it->func) == "NvAPI_DISP_GetDisplayIdByDisplayName")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_DISP_GetDisplayIdByDisplayName));
-        else if (std::string(it->func) == "NvAPI_GetErrorMessage")
-            registry.insert(std::make_pair(id, (NvAPI_Method) NvAPI_GetErrorMessage));
-        else if (std::string(it->func) == "NvAPI_Unload")
-            registry.insert(std::make_pair(id, NvAPI_Unload));
-        else if (std::string(it->func) == "NvAPI_Initialize")
-            registry.insert(std::make_pair(id, NvAPI_Initialize));
+        }
+        else INSERT_WHEN_EQUALS(NvAPI_D3D11_SetDepthBoundsTest)
+        else INSERT_WHEN_EQUALS(NvAPI_D3D11_IsNvShaderExtnOpCodeSupported)
+        else INSERT_WHEN_EQUALS(NvAPI_D3D_GetObjectHandleForResource)
+        else INSERT_WHEN_EQUALS(NvAPI_D3D_SetResourceHint)
+        else INSERT_WHEN_EQUALS(NvAPI_D3D_GetCurrentSLIState)
+        else INSERT_WHEN_EQUALS(NvAPI_DISP_GetDisplayIdByDisplayName)
+        else INSERT_WHEN_EQUALS(NvAPI_GetErrorMessage)
+        else INSERT_WHEN_EQUALS(NvAPI_Unload)
+        else INSERT_WHEN_EQUALS(NvAPI_Initialize)
         else {
             std::cerr << "NvAPI_QueryInterface " << it->func << ": Called with not implemented method" << std::endl;
             registry.insert(std::make_pair(id, (NvAPI_Method) nullptr));
@@ -51,3 +48,5 @@ extern "C" {
         return reinterpret_cast<void*>(registry.find(id)->second);
     }
 }
+
+#undef INSERT_WHEN_EQUALS
