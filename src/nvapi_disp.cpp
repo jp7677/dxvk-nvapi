2fc663ec463db4a38191cfdf5bf2512ff110c71d
Jens Peters <jp7677@gmail.com>
2020-09-12 14:39:20 +0200

    Report no HDR capabilities.

--- src/nvapi_disp.cpp
+++ src/nvapi_disp.cpp
@@ -2,7 +2,21 @@ extern "C" {
     using namespace dxvk;
 
     NvAPI_Status __cdecl NvAPI_Disp_GetHdrCapabilities(NvU32 displayId, NV_HDR_CAPABILITIES *pHdrCapabilities) {
-        return NoImplementation(str::format("NvAPI_Disp_GetHdrCapabilities ", displayId));
+        if (pHdrCapabilities->version != NV_HDR_CAPABILITIES_VER1 && pHdrCapabilities->version != NV_HDR_CAPABILITIES_VER2)
+            return IncompatibleStructVersion(str::format("NvAPI_Disp_GetHdrCapabilities ", displayId));
+
+        pHdrCapabilities->isST2084EotfSupported = false;
+        pHdrCapabilities->isTraditionalHdrGammaSupported = false;
+        pHdrCapabilities->isEdrSupported = false;
+        pHdrCapabilities->driverExpandDefaultHdrParameters = false;
+        pHdrCapabilities->isTraditionalSdrGammaSupported = false;
+        // pHdrCapabilities->display_data
+
+        if (pHdrCapabilities->version == NV_HDR_CAPABILITIES_VER2)
+            pHdrCapabilities->isDolbyVisionSupported = false;
+            // pHdrCapabilities->dv_static_metadata
+
+        return Ok(str::format("NvAPI_Disp_GetHdrCapabilities ", displayId));
     }
 
     NvAPI_Status __cdecl NvAPI_DISP_GetDisplayIdByDisplayName(const char *displayName, NvU32* displayId) {
