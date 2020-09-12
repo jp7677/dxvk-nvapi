22426d15f24d9121910de16448ddc25f412d1151
Jens Peters <jp7677@gmail.com>
2020-09-12 14:20:20 +0200

    Implement NvAPI_D3D_GetCurrentSLIState. Politely report that there is no SLI setup.

--- src/nvapi_d3d.cpp
+++ src/nvapi_d3d.cpp
@@ -22,6 +22,20 @@ extern "C" {
     }
 
     NvAPI_Status __cdecl NvAPI_D3D_GetCurrentSLIState(IUnknown* pDevice, NV_GET_CURRENT_SLI_STATE* pSliState) {
-        return NoImplementation("NvAPI_D3D_GetCurrentSLIState");
+        if (pSliState->version != NV_GET_CURRENT_SLI_STATE_VER1 && pSliState->version != NV_GET_CURRENT_SLI_STATE_VER2)
+            return IncompatibleStructVersion("NvAPI_D3D_GetCurrentSLIState");
+
+        // Report that SLI is not enabled
+        pSliState->maxNumAFRGroups = 1;
+        pSliState->numAFRGroups = 1;
+        pSliState->currentAFRIndex = 0;
+        pSliState->nextFrameAFRIndex = 0;
+        pSliState->previousFrameAFRIndex = 0;
+        pSliState->bIsCurAFRGroupNew = false;
+
+        if (pSliState->version == NV_GET_CURRENT_SLI_STATE_VER2)
+            pSliState->numVRSLIGpus = 0;
+
+        return Ok("NvAPI_D3D_GetCurrentSLIState");
     }
 }
