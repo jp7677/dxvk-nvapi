ac9f17ed7a6d202aaa9b0e6ceb31c220f6398eb4
Jens Peters <jp7677@gmail.com>
2021-03-02 20:07:12 +0100

    Formatting.

--- src/nvapi_sys.cpp
+++ src/nvapi_sys.cpp
@@ -6,21 +6,6 @@
 extern "C" {
     using namespace dxvk;
 
-    NvAPI_Status __cdecl NvAPI_SYS_GetDriverAndBranchVersion(NvU32* pDriverVersion, NvAPI_ShortString szBuildBranchString) {
-        constexpr auto n = "NvAPI_SYS_GetDriverAndBranchVersion";
-
-        if (nvapiAdapterRegistry == nullptr)
-            return ApiNotInitialized(n);
-
-        if (pDriverVersion == nullptr || szBuildBranchString == nullptr)
-            return InvalidArgument(n);
-
-        *pDriverVersion = nvapiAdapterRegistry->GetAdapter()->GetDriverVersion();
-        strcpy(szBuildBranchString, DXVK_NVAPI_VERSION);
-
-        return Ok(n);
-    }
-
     NvAPI_Status __cdecl NvAPI_SYS_GetPhysicalGpuFromDisplayId(NvU32 displayId, NvPhysicalGpuHandle *hPhysicalGpu) {
         constexpr auto n = "NvAPI_SYS_GetPhysicalGpuFromDisplayId";
 
@@ -32,6 +17,21 @@ extern "C" {
             return InvalidArgument(n);
 
         *hPhysicalGpu = (NvPhysicalGpuHandle) output->GetParent();
+
+        return Ok(n);
+    }
+
+    NvAPI_Status __cdecl NvAPI_SYS_GetDriverAndBranchVersion(NvU32* pDriverVersion, NvAPI_ShortString szBuildBranchString) {
+        constexpr auto n = "NvAPI_SYS_GetDriverAndBranchVersion";
+
+        if (nvapiAdapterRegistry == nullptr)
+            return ApiNotInitialized(n);
+
+        if (pDriverVersion == nullptr || szBuildBranchString == nullptr)
+            return InvalidArgument(n);
+
+        *pDriverVersion = nvapiAdapterRegistry->GetAdapter()->GetDriverVersion();
+        strcpy(szBuildBranchString, DXVK_NVAPI_VERSION);
 
         return Ok(n);
     }
