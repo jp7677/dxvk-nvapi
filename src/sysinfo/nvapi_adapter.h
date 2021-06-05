c55ce0a7b7118deeb485bdf2cbdd6b2c9be680c8
Jens Peters <jp7677@gmail.com>
2021-06-05 12:05:23 +0200

    Formatting.

--- src/sysinfo/nvapi_adapter.h
+++ src/sysinfo/nvapi_adapter.h
@@ -21,7 +21,7 @@ namespace dxvk {
         [[nodiscard]] uint32_t GetGpuType() const;
         [[nodiscard]] uint32_t GetBusId() const;
         [[nodiscard]] uint32_t GetVRamSize() const;
-        [[nodiscard]] bool GetLUID(LUID *luid) const;
+        [[nodiscard]] bool GetLUID(LUID* luid) const;
         [[nodiscard]] NV_GPU_ARCHITECTURE_ID GetArchitectureId() const;
 
     private:
