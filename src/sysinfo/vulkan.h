c0f6a0a0011deeb2b558a212f6a3daf381577b97
Jens Peters <jp7677@gmail.com>
2021-06-14 10:32:16 +0200

    vulkan: Use nodiscard, make final

--- src/sysinfo/vulkan.h
+++ src/sysinfo/vulkan.h
@@ -3,14 +3,14 @@
 #include "../nvapi_private.h"
 
 namespace dxvk {
-    class Vulkan {
+    class Vulkan final {
 
     public:
         Vulkan();
         ~Vulkan();
 
-        bool IsAvailable();
-        std::set<std::string> GetDeviceExtensions(VkInstance vkInstance, VkPhysicalDevice vkDevice);
+        [[nodiscard]] bool IsAvailable();
+        [[nodiscard]] std::set<std::string> GetDeviceExtensions(VkInstance vkInstance, VkPhysicalDevice vkDevice);
         void GetPhysicalDeviceProperties2(VkInstance vkInstance, VkPhysicalDevice vkDevice, VkPhysicalDeviceProperties2* deviceProperties2);
         void GetPhysicalDeviceMemoryProperties2(VkInstance vkInstance, VkPhysicalDevice vkDevice, VkPhysicalDeviceMemoryProperties2* memoryProperties2);
 
