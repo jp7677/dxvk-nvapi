a8d3cfcbf5f616b3c06ec67a690ca99a137e4d25
rochaudhari <rochaudhari@nvidia.com>
2021-06-17 15:24:02 +0530

    nvapi: Add implementations for new APIs required for DLSS D3D12. tests: Adds tests corresponding to new APIs.
    Signed-off-by: Roshan Chaudhari <rochaudhari@nvidia.com>

--- tests/nvapi_d3d12.cpp
+++ tests/nvapi_d3d12.cpp
@@ -9,10 +9,165 @@ using namespace trompeloeil;
 
 TEST_CASE("D3D12 methods succeed", "[.d3d12]") {
     D3D12DeviceMock device;
+    D3D12DeviceExtMock deviceExt;
+    D3D12GraphicsCommandListExtMock cmdListExt;
+    auto deviceRefCount = 0;
+    auto cmdListRefCount = 0;
+
+    ALLOW_CALL(deviceExt, QueryInterface(ID3D12DeviceExt::guid, _))
+        .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12DeviceExt*>(&deviceExt))
+        .LR_SIDE_EFFECT(deviceRefCount++)
+        .RETURN(S_OK);
+    ALLOW_CALL(deviceExt, AddRef())
+        .LR_SIDE_EFFECT(deviceRefCount++)
+        .RETURN(deviceRefCount);
+    ALLOW_CALL(deviceExt, Release())
+        .LR_SIDE_EFFECT(deviceRefCount--)
+        .RETURN(deviceRefCount);
+
+    ALLOW_CALL(cmdListExt, QueryInterface(ID3D12GraphicsCommandListExt::guid, _))
+        .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12GraphicsCommandListExt*>(&cmdListExt))
+        .LR_SIDE_EFFECT(cmdListRefCount++)
+        .RETURN(S_OK);
+    ALLOW_CALL(cmdListExt, AddRef())
+        .LR_SIDE_EFFECT(cmdListRefCount++)
+        .RETURN(cmdListRefCount);
+    ALLOW_CALL(cmdListExt, Release())
+        .LR_SIDE_EFFECT(cmdListRefCount--)
+        .RETURN(cmdListRefCount);
+
+    ALLOW_CALL(device, QueryInterface(ID3D12DeviceExt::guid, _))
+        .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12DeviceExt*>(&deviceExt))
+        .RETURN(E_NOINTERFACE);
+
+    SECTION("D3D12 methods without vkd3d-proton return error") {
+        
+        ALLOW_CALL(deviceExt, GetExtensionSupport(D3D12_VK_NVX_BINARY_IMPORT))
+            .RETURN(true);
+
+        FORBID_CALL(deviceExt, CreateCubinComputeShaderWithName(_, _, _, _, _, _, _));
+        FORBID_CALL(deviceExt, DestroyCubinComputeShader(_));
+        FORBID_CALL(deviceExt, GetCudaTextureObject(_, _, _));
+        FORBID_CALL(deviceExt, GetCudaSurfaceObject(_, _));
+        FORBID_CALL(deviceExt, CaptureUAVInfo(_));
+        FORBID_CALL(cmdListExt, LaunchCubinShader(_, _, _, _, _, _));
+
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShaderWithName(static_cast<ID3D12Device*>(&device), nullptr, 0, 0, 0, 0, "shader_name", nullptr) == NVAPI_ERROR);
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShader(static_cast<ID3D12Device*>(&device), nullptr, 0, 0, 0, 0, nullptr) == NVAPI_ERROR);
+
+        REQUIRE(NvAPI_D3D12_DestroyCubinComputeShader(static_cast<ID3D12Device*>(&device), nullptr) == NVAPI_ERROR);
+        D3D12_CPU_DESCRIPTOR_HANDLE srvHandle;
+
+        REQUIRE(NvAPI_D3D12_GetCudaTextureObject(static_cast<ID3D12Device*>(&device), srvHandle, srvHandle, nullptr)  == NVAPI_ERROR);
+        REQUIRE(NvAPI_D3D12_GetCudaSurfaceObject(static_cast<ID3D12Device*>(&device), srvHandle, nullptr)  == NVAPI_ERROR);
+
+        REQUIRE(NvAPI_D3D12_CaptureUAVInfo(static_cast<ID3D12Device*>(&device), nullptr) == NVAPI_ERROR);
+    }
+
+    SECTION("D3D12 methods without cubin extension return error") {
+        ALLOW_CALL(deviceExt, GetExtensionSupport(D3D12_VK_NVX_BINARY_IMPORT))
+            .RETURN(false);
+
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShaderWithName(static_cast<ID3D12Device*>(&device), nullptr, 0, 0, 0, 0, "shader_name", nullptr) == NVAPI_ERROR);
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShader(static_cast<ID3D12Device*>(&device), nullptr, 0, 0, 0, 0, nullptr) == NVAPI_ERROR);
+
+        REQUIRE(NvAPI_D3D12_DestroyCubinComputeShader(static_cast<ID3D12Device*>(&device), nullptr) == NVAPI_ERROR);
+        D3D12_CPU_DESCRIPTOR_HANDLE srvHandle;
+
+        REQUIRE(NvAPI_D3D12_GetCudaTextureObject(static_cast<ID3D12Device*>(&device), srvHandle, srvHandle, nullptr)  == NVAPI_ERROR);
+        REQUIRE(NvAPI_D3D12_GetCudaSurfaceObject(static_cast<ID3D12Device*>(&device), srvHandle, nullptr)  == NVAPI_ERROR);
+
+        REQUIRE(NvAPI_D3D12_CaptureUAVInfo(static_cast<ID3D12Device*>(&device), nullptr) == NVAPI_ERROR);
+        
+        bool isPTXSupported;
+        REQUIRE(NvAPI_D3D12_IsFatbinPTXSupported(static_cast<ID3D12Device*>(&device), &isPTXSupported) == NVAPI_ERROR);
+    }
 
     SECTION("IsNvShaderExtnOpCodeSupported with device returns OK") {
         bool supported = true;
         REQUIRE(NvAPI_D3D12_IsNvShaderExtnOpCodeSupported(&device, 1U, &supported) == NVAPI_OK);
         REQUIRE(supported == false);
+    }
+
+    SECTION("NvAPI_D3D12_GetGraphicsCapabilities returns ok") {
+        NV_D3D12_GRAPHICS_CAPS graphicsCaps;
+        graphicsCaps.bFastUAVClearSupported = false;
+        REQUIRE(NvAPI_D3D12_GetGraphicsCapabilities(static_cast<ID3D12Device*>(&device), NV_D3D12_GRAPHICS_CAPS_VER1, &graphicsCaps) == NVAPI_OK);
+        REQUIRE(graphicsCaps.bFastUAVClearSupported == true);
+    }
+
+    SECTION("D3D12 methods with vkd3d-proton returns OK ") {
+        REQUIRE_CALL(device, QueryInterface(ID3D12DeviceExt::guid, _))
+            .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12DeviceExt*>(&deviceExt))
+            .RETURN(S_OK)
+            .TIMES(1);
+
+        ALLOW_CALL(deviceExt, GetExtensionSupport(D3D12_VK_NVX_BINARY_IMPORT))
+            .RETURN(true);
+        
+        const void* cubin_data = nullptr;
+        UINT32 cubin_size = 2;
+        UINT32 block_x = 3;
+        UINT32 block_y = 4;
+        UINT32 block_z = 5;
+        const char* shader_name = "";
+        D3D12_CUBIN_DATA_HANDLE* shader_handle = reinterpret_cast<D3D12_CUBIN_DATA_HANDLE*>(0x912122);
+        D3D12_CUBIN_DATA_HANDLE** handle = &shader_handle;
+
+        ALLOW_CALL(deviceExt, CreateCubinComputeShaderWithName(cubin_data, cubin_size, block_x, block_y, block_z, shader_name, handle))
+            .RETURN(S_OK);
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShaderWithName(static_cast<ID3D12Device*>(&device), cubin_data, cubin_size, block_x, block_y, block_z, shader_name, reinterpret_cast<NVDX_ObjectHandle*>(handle)) == NVAPI_OK);
+        REQUIRE(NvAPI_D3D12_CreateCubinComputeShader(static_cast<ID3D12Device*>(&device), cubin_data, cubin_size, block_x, block_y, block_z, reinterpret_cast<NVDX_ObjectHandle*>(handle)) == NVAPI_OK);
+
+        ALLOW_CALL(deviceExt, DestroyCubinComputeShader(shader_handle))
+            .RETURN(S_OK);
+        REQUIRE(NvAPI_D3D12_DestroyCubinComputeShader(static_cast<ID3D12Device*>(&device), reinterpret_cast<NVDX_ObjectHandle>(shader_handle)) == NVAPI_OK);
+
+        const D3D12_CPU_DESCRIPTOR_HANDLE srvHandle = {0};
+        D3D12_CPU_DESCRIPTOR_HANDLE samplerHandle = {0};
+        ALLOW_CALL(deviceExt, GetCudaTextureObject(_, _, nullptr))
+                .RETURN(S_OK);
+        REQUIRE(NvAPI_D3D12_GetCudaTextureObject(static_cast<ID3D12Device*>(&device), srvHandle, samplerHandle, nullptr)  == NVAPI_OK);
+
+        ALLOW_CALL(deviceExt, GetCudaSurfaceObject(_, nullptr))
+            .RETURN(S_OK);
+        REQUIRE(NvAPI_D3D12_GetCudaSurfaceObject(static_cast<ID3D12Device*>(&device), srvHandle, nullptr)  == NVAPI_OK);
+
+        ALLOW_CALL(deviceExt, CaptureUAVInfo(nullptr))
+            .RETURN(1);
+        REQUIRE(NvAPI_D3D12_CaptureUAVInfo(static_cast<ID3D12Device*>(&device), nullptr) == NVAPI_OK);
+
+        bool isPTXSupported = false;
+        REQUIRE(NvAPI_D3D12_IsFatbinPTXSupported(static_cast<ID3D12Device*>(&device), &isPTXSupported) == NVAPI_OK);
+        REQUIRE(isPTXSupported == true);
+    }
+
+    SECTION("LaunchCubinShader returns error when vkd3d-proton disabled ") {
+        D3D12GraphicsCommandListMock cmdList;
+        ALLOW_CALL(cmdList, QueryInterface(ID3D12GraphicsCommandListExt::guid, _))
+            .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12GraphicsCommandListExt*>(&cmdListExt))
+            .RETURN(E_NOINTERFACE);
+        
+        FORBID_CALL(cmdListExt, LaunchCubinShader(_, _, _, _, _, _));
+        REQUIRE(NvAPI_D3D12_LaunchCubinShader(static_cast<ID3D12GraphicsCommandList*>(&cmdList), reinterpret_cast<NVDX_ObjectHandle>(0), 0, 0, 0, nullptr, 0) == NVAPI_ERROR);
+    }
+    
+    SECTION("LaunchCubinShader returns ok when vkd3d-proton enabled ") {
+        D3D12GraphicsCommandListMock cmdList;
+        ALLOW_CALL(cmdList, QueryInterface(ID3D12GraphicsCommandListExt::guid, _))
+            .LR_SIDE_EFFECT(*_2 = static_cast<ID3D12GraphicsCommandListExt*>(&cmdListExt))
+            .RETURN(S_OK);
+
+        NVDX_ObjectHandle pShader = nullptr;
+        NvU32 blockX = 0;
+        NvU32 blockY = 0;
+        NvU32 blockZ = 0;
+        const void* params = nullptr;
+        NvU32 paramSize = 0;
+
+        ALLOW_CALL(cmdListExt, LaunchCubinShader(reinterpret_cast<D3D12_CUBIN_DATA_HANDLE*>(pShader), blockX, blockY, blockZ, params, paramSize))
+            .RETURN(S_OK);
+
+        REQUIRE(NvAPI_D3D12_LaunchCubinShader(static_cast<ID3D12GraphicsCommandList*>(&cmdList), pShader, blockX, blockY, blockZ, params, paramSize) == NVAPI_OK);
     }
 }
--- tests/nvapi_d3d12_mocks.cpp
+++ tests/nvapi_d3d12_mocks.cpp
@@ -3,7 +3,7 @@
 using namespace trompeloeil;
 
 class D3D12DeviceMock : public mock_interface<ID3D12Device> {
-    MAKE_MOCK2(QueryInterface, HRESULT(REFIID, void**), override);
+    MAKE_MOCK2(QueryInterface, HRESULT(REFIID, void **), override);
     MAKE_MOCK0(AddRef, ULONG(), override);
     MAKE_MOCK0(Release, ULONG(), override);
     IMPLEMENT_MOCK3(GetPrivateData);
@@ -47,4 +47,88 @@ class D3D12DeviceMock : public mock_interface<ID3D12Device> {
     IMPLEMENT_MOCK4(CreateCommandSignature);
     IMPLEMENT_MOCK7(GetResourceTiling);
     MAKE_MOCK1(GetAdapterLuid, LUID*(LUID*), override);
+};
+
+class D3D12DeviceExtMock : public mock_interface<ID3D12DeviceExt> {
+    MAKE_MOCK2(QueryInterface, HRESULT(REFIID, void **), override);
+    MAKE_MOCK0(AddRef, ULONG(), override);
+    MAKE_MOCK0(Release, ULONG(), override);
+    IMPLEMENT_MOCK3(GetVulkanHandles);
+    IMPLEMENT_MOCK1(GetExtensionSupport);
+    IMPLEMENT_MOCK7(CreateCubinComputeShaderWithName);
+    IMPLEMENT_MOCK1(DestroyCubinComputeShader);
+    IMPLEMENT_MOCK3(GetCudaTextureObject);
+    IMPLEMENT_MOCK2(GetCudaSurfaceObject);
+    IMPLEMENT_MOCK1(CaptureUAVInfo);
+};
+
+class D3D12GraphicsCommandListMock : public mock_interface<ID3D12GraphicsCommandList> {
+    MAKE_MOCK2(QueryInterface, HRESULT(REFIID, void **), override);
+    MAKE_MOCK0(AddRef, ULONG(), override);
+    MAKE_MOCK0(Release, ULONG(), override);
+    IMPLEMENT_MOCK3(GetPrivateData);
+    IMPLEMENT_MOCK3(SetPrivateData);
+    IMPLEMENT_MOCK2(SetPrivateDataInterface);
+    IMPLEMENT_MOCK1(SetName);
+    IMPLEMENT_MOCK2(GetDevice);
+    IMPLEMENT_MOCK0(GetType);
+    IMPLEMENT_MOCK6(ExecuteIndirect);
+    IMPLEMENT_MOCK0(EndEvent);
+    IMPLEMENT_MOCK3(BeginEvent);
+    IMPLEMENT_MOCK3(SetMarker);
+    IMPLEMENT_MOCK6(ResolveQueryData);
+    IMPLEMENT_MOCK3(EndQuery);
+    IMPLEMENT_MOCK3(BeginQuery);
+    IMPLEMENT_MOCK2(DiscardResource);
+    IMPLEMENT_MOCK6(ClearUnorderedAccessViewFloat);
+    IMPLEMENT_MOCK6(ClearUnorderedAccessViewUint);
+    IMPLEMENT_MOCK4(ClearRenderTargetView);
+    IMPLEMENT_MOCK6(ClearDepthStencilView);
+    IMPLEMENT_MOCK4(OMSetRenderTargets);
+    IMPLEMENT_MOCK3(SOSetTargets);
+    IMPLEMENT_MOCK3(IASetVertexBuffers);
+    IMPLEMENT_MOCK1(IASetIndexBuffer);
+    IMPLEMENT_MOCK2(SetGraphicsRootUnorderedAccessView);
+    IMPLEMENT_MOCK2(SetComputeRootUnorderedAccessView);
+    IMPLEMENT_MOCK2(SetGraphicsRootShaderResourceView);
+    IMPLEMENT_MOCK2(SetComputeRootShaderResourceView);
+    IMPLEMENT_MOCK2(SetGraphicsRootConstantBufferView);
+    IMPLEMENT_MOCK2(SetComputeRootConstantBufferView);
+    IMPLEMENT_MOCK4(SetGraphicsRoot32BitConstants);
+    IMPLEMENT_MOCK4(SetComputeRoot32BitConstants);
+    IMPLEMENT_MOCK3(SetGraphicsRoot32BitConstant);
+    IMPLEMENT_MOCK3(SetComputeRoot32BitConstant);
+    IMPLEMENT_MOCK2(SetGraphicsRootDescriptorTable);
+    IMPLEMENT_MOCK2(SetComputeRootDescriptorTable);
+    IMPLEMENT_MOCK1(SetGraphicsRootSignature);
+    IMPLEMENT_MOCK1(SetComputeRootSignature);
+    IMPLEMENT_MOCK2(SetDescriptorHeaps);
+    IMPLEMENT_MOCK1(ExecuteBundle);
+    IMPLEMENT_MOCK2(ResourceBarrier);
+    IMPLEMENT_MOCK1(SetPipelineState);
+    IMPLEMENT_MOCK1(OMSetStencilRef);
+    IMPLEMENT_MOCK1(OMSetBlendFactor);
+    IMPLEMENT_MOCK2(RSSetScissorRects);
+    IMPLEMENT_MOCK2(RSSetViewports);
+    IMPLEMENT_MOCK1(IASetPrimitiveTopology);
+    IMPLEMENT_MOCK5(ResolveSubresource);
+    IMPLEMENT_MOCK6(CopyTiles);
+    IMPLEMENT_MOCK2(CopyResource);
+    IMPLEMENT_MOCK6(CopyTextureRegion);
+    IMPLEMENT_MOCK5(CopyBufferRegion);
+    IMPLEMENT_MOCK3(Dispatch);
+    IMPLEMENT_MOCK5(DrawIndexedInstanced);
+    IMPLEMENT_MOCK4(DrawInstanced);
+    IMPLEMENT_MOCK1(ClearState);
+    IMPLEMENT_MOCK2(Reset);
+    IMPLEMENT_MOCK0(Close);
+    IMPLEMENT_MOCK3(SetPredication);
+};
+
+class D3D12GraphicsCommandListExtMock : public mock_interface<ID3D12GraphicsCommandListExt> {
+    MAKE_MOCK2(QueryInterface, HRESULT(REFIID, void **), override);
+    MAKE_MOCK0(AddRef, ULONG(), override);
+    MAKE_MOCK0(Release, ULONG(), override);
+    IMPLEMENT_MOCK1(GetVulkanHandle);
+    IMPLEMENT_MOCK6(LaunchCubinShader);
 };
