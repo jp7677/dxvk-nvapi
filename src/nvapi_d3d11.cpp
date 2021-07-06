656ab8be4fcd48456e6f24b77a6c7ad94eb819f3
Jens Peters <jp7677@gmail.com>
2021-07-06 20:53:19 +0200

    nvapi-d3d11: Improve logging Do not hide error logging when earlier call succeeded.

--- src/nvapi_d3d11.cpp
+++ src/nvapi_d3d11.cpp
@@ -9,67 +9,72 @@ extern "C" {
 
     NvAPI_Status __cdecl NvAPI_D3D11_SetDepthBoundsTest(IUnknown *pDeviceOrContext, NvU32 bEnable, float fMinDepth, float fMaxDepth) {
         constexpr auto n = "NvAPI_D3D11_SetDepthBoundsTest";
-        static bool alreadyLogged = false;
+        static bool alreadyLoggedError = false;
+        static bool alreadyLoggedOk = false;
 
         if (pDeviceOrContext == nullptr)
             return InvalidArgument(n);
 
         if (!NvapiD3d11Device::SetDepthBoundsTest(pDeviceOrContext, bEnable, fMinDepth, fMaxDepth))
-            return Error(n, alreadyLogged);
+            return Error(n, alreadyLoggedError);
 
-        return Ok(n, alreadyLogged);
+        return Ok(n, alreadyLoggedOk);
     }
 
     NvAPI_Status __cdecl NvAPI_D3D11_BeginUAVOverlap(IUnknown *pDeviceOrContext) {
         constexpr auto n = "NvAPI_D3D11_BeginUAVOverlap";
-        static bool alreadyLogged = false;
+        static bool alreadyLoggedError = false;
+        static bool alreadyLoggedOk = false;
 
         if (pDeviceOrContext == nullptr)
             return InvalidArgument(n);
 
         if (!NvapiD3d11Device::BeginUAVOverlap(pDeviceOrContext))
-            return Error(n, alreadyLogged);
+            return Error(n, alreadyLoggedError);
 
-        return Ok(n, alreadyLogged);
+        return Ok(n, alreadyLoggedOk);
     }
 
     NvAPI_Status __cdecl NvAPI_D3D11_EndUAVOverlap(IUnknown *pDeviceOrContext) {
         constexpr auto n = "NvAPI_D3D11_EndUAVOverlap";
-        static bool alreadyLogged = false;
+        static bool alreadyLoggedError = false;
+        static bool alreadyLoggedOk = false;
 
         if (pDeviceOrContext == nullptr)
             return InvalidArgument(n);
 
         if (!NvapiD3d11Device::EndUAVOverlap(pDeviceOrContext))
-            return Error(n, alreadyLogged);
+            return Error(n, alreadyLoggedError);
 
-        return Ok(n, alreadyLogged);
+        return Ok(n, alreadyLoggedOk);
     }
 
     NvAPI_Status __cdecl NvAPI_D3D11_MultiDrawInstancedIndirect(ID3D11DeviceContext *pDevContext11, NvU32 drawCount, ID3D11Buffer *pBuffer, NvU32 alignedByteOffsetForArgs, NvU32 alignedByteStrideForArgs) {
         constexpr auto n = "NvAPI_D3D11_MultiDrawInstancedIndirect";
-        static bool alreadyLogged = false;
+        static bool alreadyLoggedError = false;
+        static bool alreadyLoggedOk = false;
 
         if (pDevContext11 == nullptr || pBuffer == nullptr)
             return InvalidArgument(n);
 
         if (!NvapiD3d11Device::MultiDrawInstancedIndirect(pDevContext11, drawCount, pBuffer, alignedByteOffsetForArgs, alignedByteStrideForArgs))
-            return Error(n, alreadyLogged);
+            return Error(n, alreadyLoggedError);
 
-        return Ok(n, alreadyLogged);
+        return Ok(n, alreadyLoggedOk);
     }
 
     NvAPI_Status __cdecl NvAPI_D3D11_MultiDrawIndexedInstancedIndirect(ID3D11DeviceContext *pDevContext11, NvU32 drawCount, ID3D11Buffer *pBuffer, NvU32 alignedByteOffsetForArgs, NvU32 alignedByteStrideForArgs) {
         constexpr auto n = "NvAPI_D3D11_MultiDrawIndexedInstancedIndirect";
-        static bool alreadyLogged = false;
+        static bool alreadyLoggedError = false;
+        static bool alreadyLoggedOk = false;
 
         if (pDevContext11 == nullptr || pBuffer == nullptr)
             return InvalidArgument(n);
 
         if (!NvapiD3d11Device::MultiDrawIndexedInstancedIndirect(pDevContext11, drawCount, pBuffer, alignedByteOffsetForArgs, alignedByteStrideForArgs))
-            return Error(n, alreadyLogged);
+            return Error(n, alreadyLoggedError);
 
-        return Ok(n, alreadyLogged);
+        return Ok(n, alreadyLoggedOk);
     }
 
     NvAPI_Status __cdecl NvAPI_D3D11_IsNvShaderExtnOpCodeSupported(IUnknown *pDeviceOrContext, NvU32 code, bool *supported) {
