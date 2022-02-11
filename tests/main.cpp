b19df20aec9cd76ea36f8f6f9b3974956b36b856
Jens Peters <jp7677@gmail.com>
2022-02-11 14:53:38 +0100

    tests: Add DRS tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "../inc/catch.hpp"
 
-CATCH_REGISTER_TAG_ALIAS("[@unit-tests]", "[d3d],[d3d11],[d3d12],[sysinfo]")
+CATCH_REGISTER_TAG_ALIAS("[@unit-tests]", "[d3d],[d3d11],[d3d12],[drs],[sysinfo]")
 CATCH_REGISTER_TAG_ALIAS("[@system]", "[system]")
-CATCH_REGISTER_TAG_ALIAS("[@all]", "[d3d],[d3d11],[d3d12],[sysinfo],[system]")
+CATCH_REGISTER_TAG_ALIAS("[@all]", "[d3d],[d3d11],[d3d12],[drs],[sysinfo],[system]")
