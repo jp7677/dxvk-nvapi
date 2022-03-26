deeba94c5c2660f0f942cd2210b4999a6e725e96
Jens Peters <jp7677@gmail.com>
2022-03-26 10:29:55 +0100

    util: Add space for termination See https://github.com/jp7677/dxvk-nvapi/pull/78#issuecomment-1079648200

--- src/util/util_string.cpp
+++ src/util/util_string.cpp
@@ -37,7 +37,7 @@ namespace dxvk::str {
     }
 
     void tonvss(NvAPI_ShortString nvss, std::string str) {
-        str.resize(NVAPI_SHORT_STRING_MAX);
+        str.resize(NVAPI_SHORT_STRING_MAX - 1);
         strcpy(nvss, str.c_str());
     }
 }
