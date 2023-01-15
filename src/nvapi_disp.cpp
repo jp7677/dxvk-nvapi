7770d881ac05a0ccd965b83f1d31604eb415450b
Jens Peters <jp7677@gmail.com>
2023-01-15 11:19:01 +0100

    nvapi-disp: Initialize all fields

--- src/nvapi_disp.cpp
+++ src/nvapi_disp.cpp
@@ -24,36 +24,15 @@ extern "C" {
         // Reference (a.o.): https://www.asawicki.info/news_1703_programming_hdr_monitor_support_in_direct3d
         switch (pHdrCapabilities->version) {
             case NV_HDR_CAPABILITIES_VER1: {
-                auto pHdrCapabilitiesV1 = reinterpret_cast<NV_HDR_CAPABILITIES_V1*>(pHdrCapabilities);
-                pHdrCapabilitiesV1->isST2084EotfSupported = false;
-                pHdrCapabilitiesV1->isTraditionalHdrGammaSupported = false;
-                pHdrCapabilitiesV1->isEdrSupported = false;
-                pHdrCapabilitiesV1->driverExpandDefaultHdrParameters = false;
-                pHdrCapabilitiesV1->isTraditionalSdrGammaSupported = false;
-                // pHdrCapabilities->display_data
+                memset(pHdrCapabilities, 0, sizeof(NV_HDR_CAPABILITIES_V1));
                 break;
             }
             case NV_HDR_CAPABILITIES_VER2: {
-                auto pHdrCapabilitiesV2 = reinterpret_cast<NV_HDR_CAPABILITIES_V2*>(pHdrCapabilities);
-                pHdrCapabilitiesV2->isST2084EotfSupported = false;
-                pHdrCapabilitiesV2->isTraditionalHdrGammaSupported = false;
-                pHdrCapabilitiesV2->isEdrSupported = false;
-                pHdrCapabilitiesV2->driverExpandDefaultHdrParameters = false;
-                pHdrCapabilitiesV2->isTraditionalSdrGammaSupported = false;
-                // pHdrCapabilities->display_data
-                pHdrCapabilitiesV2->isDolbyVisionSupported = false;
-                // pHdrCapabilities->dv_static_metadata
+                memset(pHdrCapabilities, 0, sizeof(NV_HDR_CAPABILITIES_V2));
                 break;
             }
             case NV_HDR_CAPABILITIES_VER3:
-                pHdrCapabilities->isST2084EotfSupported = false;
-                pHdrCapabilities->isTraditionalHdrGammaSupported = false;
-                pHdrCapabilities->isEdrSupported = false;
-                pHdrCapabilities->driverExpandDefaultHdrParameters = false;
-                pHdrCapabilities->isTraditionalSdrGammaSupported = false;
-                // pHdrCapabilities->display_data
-                pHdrCapabilities->isDolbyVisionSupported = false;
-                // pHdrCapabilities->dv_static_metadata
+                memset(pHdrCapabilities, 0, sizeof(NV_HDR_CAPABILITIES_V3));
                 break;
             default:
                 return IncompatibleStructVersion(n);
--- tests/nvapi_sysinfo.cpp
+++ tests/nvapi_sysinfo.cpp
@@ -1338,6 +1338,17 @@ TEST_CASE("GetHdrCapabilities succeeds", "[.sysinfo]") {
         REQUIRE(capabilities.isEdrSupported == false);
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
     }
 
     SECTION("GetHdrCapabilities (V2) returns OK") {
@@ -1349,6 +1360,17 @@ TEST_CASE("GetHdrCapabilities succeeds", "[.sysinfo]") {
         REQUIRE(capabilities.isEdrSupported == false);
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
     }
 
     SECTION("GetHdrCapabilities (V3) returns OK") {
@@ -1361,6 +1383,17 @@ TEST_CASE("GetHdrCapabilities succeeds", "[.sysinfo]") {
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
         REQUIRE(capabilities.isDolbyVisionSupported == false);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
     }
 
     SECTION("GetHdrCapabilities with unknown struct version returns incompatible-struct-version") {
