3df842f34221b74d24b841a6d335f0b6b67dee4a
Jens Peters <jp7677@gmail.com>
2023-01-22 11:21:42 +0100

    tests: Test color data

--- tests/nvapi_sysinfo_hdr.cpp
+++ tests/nvapi_sysinfo_hdr.cpp
@@ -16,7 +16,28 @@ TEST_CASE("HDR related Sysinfo methods succeed", "[.sysinfo-hdr]") {
     auto e = ConfigureDefaultTestEnvironment(*dxgiFactory, *vulkan, *nvml, *lfx, adapter, output);
     auto primaryDisplayId = 0x00010001;
 
-    ::SetEnvironmentVariableA("DXVK_NVAPI_DRIVER_VERSION", "");
+    auto desc1 = DXGI_OUTPUT_DESC1{
+        L"Output1",
+        {0, 0, 0, 0},
+        1,
+        DXGI_MODE_ROTATION_UNSPECIFIED,
+        nullptr,
+        8U,
+        DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709,
+        {0.640f, 0.330f},
+        {0.300f, 0.600f},
+        {0.150f, 0.060f},
+        {0.3127f, 0.3290f},
+        0.01f,
+        1499.0f,
+        799.0f};
+
+    ALLOW_CALL(output, QueryInterface(__uuidof(IDXGIOutput6), _))
+        .LR_SIDE_EFFECT(*_2 = static_cast<IDXGIOutput6*>(&output))
+        .RETURN(S_OK);
+    ALLOW_CALL(output, GetDesc1(_))
+        .SIDE_EFFECT(*_1 = desc1)
+        .RETURN(S_OK);
 
     SetupResourceFactory(std::move(dxgiFactory), std::move(vulkan), std::move(nvml), std::move(lfx));
     REQUIRE(NvAPI_Initialize() == NVAPI_OK);
@@ -30,17 +51,17 @@ TEST_CASE("HDR related Sysinfo methods succeed", "[.sysinfo-hdr]") {
         REQUIRE(capabilities.isEdrSupported == false);
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
-        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 32000);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 16500);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 15000);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 30000);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 7500);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 3000);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 15635);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 16450);
         REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 1499);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 799);
     }
 
     SECTION("GetHdrCapabilities (V2) returns OK") {
@@ -52,17 +73,17 @@ TEST_CASE("HDR related Sysinfo methods succeed", "[.sysinfo-hdr]") {
         REQUIRE(capabilities.isEdrSupported == false);
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
-        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 32000);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 16500);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 15000);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 30000);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 7500);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 3000);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 15635);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 16450);
         REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 1499);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 799);
     }
 
     SECTION("GetHdrCapabilities (V3) returns OK") {
@@ -75,17 +96,17 @@ TEST_CASE("HDR related Sysinfo methods succeed", "[.sysinfo-hdr]") {
         REQUIRE(capabilities.driverExpandDefaultHdrParameters == false);
         REQUIRE(capabilities.isTraditionalSdrGammaSupported == false);
         REQUIRE(capabilities.isDolbyVisionSupported == false);
-        REQUIRE(capabilities.display_data.displayPrimary_x0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y0 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y1 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_x2 == 0);
-        REQUIRE(capabilities.display_data.displayPrimary_y2 == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_x == 0);
-        REQUIRE(capabilities.display_data.displayWhitePoint_y == 0);
+        REQUIRE(capabilities.display_data.displayPrimary_x0 == 32000);
+        REQUIRE(capabilities.display_data.displayPrimary_y0 == 16500);
+        REQUIRE(capabilities.display_data.displayPrimary_x1 == 15000);
+        REQUIRE(capabilities.display_data.displayPrimary_y1 == 30000);
+        REQUIRE(capabilities.display_data.displayPrimary_x2 == 7500);
+        REQUIRE(capabilities.display_data.displayPrimary_y2 == 3000);
+        REQUIRE(capabilities.display_data.displayWhitePoint_x == 15635);
+        REQUIRE(capabilities.display_data.displayWhitePoint_y == 16450);
         REQUIRE(capabilities.display_data.desired_content_min_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_luminance == 0);
-        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 0);
+        REQUIRE(capabilities.display_data.desired_content_max_luminance == 1499);
+        REQUIRE(capabilities.display_data.desired_content_max_frame_average_luminance == 799);
     }
 
     SECTION("GetHdrCapabilities with unknown struct version returns incompatible-struct-version") {
