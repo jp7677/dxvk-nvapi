c7afbcea990be0d7f0299f5169f805786ae8e1c3
Jens Peters <jp7677@gmail.com>
2023-03-09 19:02:01 +0100

    build: Improve formatting
    Makes our IDE happy.

--- validate-methods.py
+++ validate-methods.py
@@ -1,15 +1,18 @@
-import sys, re, getopt
+import sys
+import re
 
 if sys.version_info[0] < 3:
-    print("Method validation failed due to invalid Python interpreter. Please use Python 3 as your default Python interpreter.")
+    print("Method validation failed due to invalid Python interpreter. "
+          "Please use Python 3 as your default Python interpreter.")
     sys.exit(1)
 
 if len(sys.argv) < 3:
-    print("Method validation failed due to invalid usage. Usage: python validate-methods.py <implementation1.cpp> [implementation2.cpp] <interface.cpp> <interface.h>")
+    print("Method validation failed due to invalid usage. "
+          "Usage: python validate-methods.py <implementation1.cpp> [implementation2.cpp] <interface.cpp> <interface.h>")
     sys.exit(1)
 
 expectedMethods = []
-regex = re.compile("^\ *NvAPI_Status\ +__cdecl\ +(\w+)")
+regex = re.compile("^ *NvAPI_Status +__cdecl +(\\w+)")
 for i in range(1, len(sys.argv) - 2):
     with open(sys.argv[i]) as file:
         for line in file:
@@ -19,7 +22,7 @@ for i in range(1, len(sys.argv) - 2):
 
 fetch = False
 foundMethods = []
-regex = re.compile("^\ *\w+\((\w+)\)")
+regex = re.compile("^ *\\w+\\((\\w+)\\)")
 with open(sys.argv[len(sys.argv) - 2]) as file:
     for line in file:
         if line.strip() == "/* End */":
@@ -32,19 +35,23 @@ with open(sys.argv[len(sys.argv) - 2]) as file:
             fetch = True
 
 availableMethods = []
-regex = re.compile("^\ *{ \\\"(\w+)\\\", \w+ },$")
+regex = re.compile("^ *{ \\\"(\\w+)\\\", \\w+ },$")
 with open(sys.argv[len(sys.argv) - 1]) as file:
     for line in file:
         result = regex.match(line)
         if result:
             availableMethods.append(result.group(1))
 
-if len(expectedMethods) != len(foundMethods) or len(set(expectedMethods).intersection(foundMethods)) != len(expectedMethods):
-    print("Method validation failed. Please make sure that all implemented NVAPI methods are also listed in the `nvapi_QueryInterface` function.")
+if len(expectedMethods) != len(foundMethods) or len(set(expectedMethods).intersection(foundMethods)) != len(
+        expectedMethods):
+    print("Method validation failed. "
+          "Please make sure that all implemented NVAPI methods are listed in the `nvapi_QueryInterface` function.")
     sys.exit(1)
 
 if not set(foundMethods).issubset(set(availableMethods)):
-    print("Method validation failed. Please make sure that all implemented NVAPI methods correspond to available methods in the `nvapi_interface_table` struct.")
+    print("Method validation failed. "
+          "Please make sure that all implemented NVAPI methods "
+          "correspond to available methods in the `nvapi_interface_table` struct.")
     sys.exit(1)
 
 sys.exit(0)
