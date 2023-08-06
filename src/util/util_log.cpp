b0ff960473cfd87215b7361266cb96d32e68bbbf
Jens Peters <jp7677@gmail.com>
2023-08-06 09:23:57 +0200

    util: Use __wine_dbg_output if available
    This restores console output in newer Wine versions,
    which was lost in some circumstances.

--- src/util/util_log.cpp
+++ src/util/util_log.cpp
@@ -1,8 +1,27 @@
 #include "util_log.h"
 #include "util_env.h"
+#include "util_string.h"
+
+using PFN_wineDbgOutput = int(__cdecl*)(const char*);
+
+static PFN_wineDbgOutput wineDbgOutput = nullptr;
 
 namespace dxvk::log {
+    void print(const std::string& message) {
+        auto line = message + '\n';
+        if (wineDbgOutput)
+            wineDbgOutput(line.c_str());
+        else
+            std::cerr << line;
+    }
+
     void initialize(std::ofstream& filestream, bool& skipAllLogging) {
+#ifdef _WIN32
+        auto ntdllModule = ::GetModuleHandleA("ntdll.dll");
+        if (ntdllModule != nullptr)
+            wineDbgOutput = reinterpret_cast<PFN_wineDbgOutput>(reinterpret_cast<void*>(GetProcAddress(ntdllModule, "__wine_dbg_output")));
+#endif
+
         constexpr auto logLevelEnvName = "DXVK_NVAPI_LOG_LEVEL";
         constexpr auto logPathEnvName = "DXVK_NVAPI_LOG_PATH";
         constexpr auto logFileName = "dxvk-nvapi.log";
@@ -23,7 +42,7 @@ namespace dxvk::log {
         auto fullPath = logPath + logFileName;
         filestream = std::ofstream(fullPath, std::ios::app);
         filestream << "---------- " << env::getCurrentDateTime() << " ----------" << std::endl;
-        std::cerr << logPathEnvName << " is set to '" << logPath << "', appending log statements to " << fullPath << std::endl;
+        print(str::format(logPathEnvName, " is set to '", logPath, "', appending log statements to ", fullPath));
     }
 
     void write(const std::string& message) {
@@ -36,7 +55,7 @@ namespace dxvk::log {
         if (skipAllLogging)
             return;
 
-        std::cerr << message << std::endl;
+        print(message);
         if (filestream)
             filestream << message << std::endl;
     }
