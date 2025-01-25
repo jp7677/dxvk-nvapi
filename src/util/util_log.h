19e23b28f61e752f70c0ac72c9f0aa7ead3a9969
Jens Peters <jp7677@gmail.com>
2025-01-25 09:14:47 +0100

    util: Shorten raw pointer checks
    More consistent with other parts of the code base.

--- src/util/util_log.h
+++ src/util/util_log.h
@@ -13,14 +13,14 @@ namespace dxvk::log {
 #endif
 
         inline std::string hnd(const void* h) {
-            if (h == nullptr)
+            if (!h)
                 return "hnd=0x0";
 
             return str::format("hnd=", hex_prefix, std::hex, h);
         }
 
         inline std::string ptr(const void* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("ptr=", hex_prefix, std::hex, p);
@@ -44,42 +44,42 @@ namespace dxvk::log {
         }
 
         inline std::string nv_latency_marker_params(NV_LATENCY_MARKER_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",frameID=", p->frameID, ",markerType=", fromLatencyMarkerType(p->markerType), ",rsvd}");
         }
 
         inline std::string nv_async_frame_marker_params(NV_ASYNC_FRAME_MARKER_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",frameID=", p->frameID, ",markerType=", fromLatencyMarkerType(p->markerType), ",presentFrameID=", p->presentFrameID, ",rsvd}");
         }
 
         inline std::string nv_vk_get_sleep_status_params(NV_VULKAN_GET_SLEEP_STATUS_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",...,rsvd}");
         }
 
         inline std::string nv_vk_set_sleep_status_params(NV_VULKAN_SET_SLEEP_MODE_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",bLowLatencyMode=", static_cast<bool>(p->bLowLatencyMode), ",bLowLatencyBoost=", static_cast<bool>(p->bLowLatencyBoost), ",minimumIntervalUs=", p->minimumIntervalUs, ",rsvd}");
         }
 
         inline std::string nv_vk_latency_result_params(NV_VULKAN_LATENCY_RESULT_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",...,rsvd}");
         }
 
         inline std::string nv_vk_latency_marker_params(NV_VULKAN_LATENCY_MARKER_PARAMS* p) {
-            if (p == nullptr)
+            if (!p)
                 return "nullptr";
 
             return str::format("{version=", p->version, ",frameID=", p->frameID, ",markerType=", p->markerType, ",rsvd}");
