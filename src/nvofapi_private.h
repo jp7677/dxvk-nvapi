88399ee92be3fc17b6a4a8ebe730c1d271466bc7
Sveinar Søpler <cybermax@dexter.no>
2025-02-02 18:18:19 +0100

    nvofapi-d3d11: Add stub for NvOFAPICreateInstanceD3D11
    Return not-available because OpticalFlow using D3D11 is not implemented.

--- src/nvofapi_private.h
+++ src/nvofapi_private.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "./nvapi_private.h"
+#include "../inc/nvofapi/nvOpticalFlowD3D11.h"
 #include "../inc/nvofapi/nvOpticalFlowD3D12.h"
 #include "../inc/nvofapi/nvOpticalFlowVulkan.h"
