84c57b425c8a866f3c50c58df08c78f70b47c8be
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2025-05-24 19:19:11 +0200

    nvapi-d3d12: Extend NvapiD3d12GraphicsCommandList for OMM and RTXMG

--- src/nvapi/nvapi_d3d12_graphics_command_list.h
+++ src/nvapi/nvapi_d3d12_graphics_command_list.h
@@ -14,11 +14,21 @@ namespace dxvk {
 
         [[nodiscard]] HRESULT LaunchCubinShader(NVDX_ObjectHandle shader, NvU32 blockX, NvU32 blockY, NvU32 blockZ, const void* params, NvU32 paramSize) const;
 
+        [[nodiscard]] NvAPI_Status BuildRaytracingAccelerationStructureEx(const NVAPI_BUILD_RAYTRACING_ACCELERATION_STRUCTURE_EX_PARAMS* params) const;
+        [[nodiscard]] NvAPI_Status BuildRaytracingOpacityMicromapArray(NVAPI_BUILD_RAYTRACING_OPACITY_MICROMAP_ARRAY_PARAMS* params) const;
+        [[nodiscard]] NvAPI_Status RelocateRaytracingOpacityMicromapArray(const NVAPI_RELOCATE_RAYTRACING_OPACITY_MICROMAP_ARRAY_PARAMS* params) const;
+        [[nodiscard]] NvAPI_Status EmitRaytracingOpacityMicromapArrayPostbuildInfo(const NVAPI_EMIT_RAYTRACING_OPACITY_MICROMAP_ARRAY_POSTBUILD_INFO_PARAMS* params) const;
+
+        [[nodiscard]] NvAPI_Status RaytracingExecuteMultiIndirectClusterOperation(const NVAPI_RAYTRACING_EXECUTE_MULTI_INDIRECT_CLUSTER_OPERATION_PARAMS* params) const;
+        [[nodiscard]] NvAPI_Status BuildRaytracingPartitionedTlasIndirect(const NVAPI_BUILD_RAYTRACING_PARTITIONED_TLAS_INDIRECT_PARAMS* params) const;
+
       private:
         static std::unordered_map<ID3D12GraphicsCommandList*, NvapiD3d12GraphicsCommandList> m_nvapiDeviceMap;
         static std::mutex m_mutex;
 
-        ID3D12GraphicsCommandListExt1* m_vkd3dGraphicsCommandList{};
+        ID3D12GraphicsCommandListExt3* m_vkd3dGraphicsCommandList{};
         bool m_supportsExtGraphicsCommandList1 = false;
+        bool m_supportsExtGraphicsCommandList2 = false;
+        bool m_supportsExtGraphicsCommandList3 = false;
     };
 }
