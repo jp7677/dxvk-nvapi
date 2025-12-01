72d595eaab0369d802d9c407276fcea79be42ddd
alltern <143025426+alltern@users.noreply.github.com>
2025-11-30 21:56:21 -0500

    Add easy build and install script.

---
+++ BuildAndInstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Add wineprefix as an argument"
+  exit
+fi
+
+WINEPREFIX=$1
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $DIR
+
+rm -rf ./builds/
+./package-release.sh master ./builds/
+
+cd ./builds/dxvk-nvapi-master/
+
+cp ./x32/* $WINEPREFIX/drive_c/windows/syswow64/
+cp ./x64/* $WINEPREFIX/drive_c/windows/system32/
+
+echo "Install layer to '~/.local/share/vulkan/implicit_layer.d/' [Y/n]"
+read -r YESNO
+if [ $YESNO = 'n' ] || [ $YESNO = 'N' ]; then
+  echo Not installing layer
+  exit
+fi
+
+echo "Installing layer to '~/.local/share/vulkan/implicit_layer.d/'"
+
+cp ./layer/* ~/.local/share/vulkan/implicit_layer.d/
